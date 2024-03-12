68f332b84ceed04f17d6ae4c294be6f3d30a4d35
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-03-13 00:19:17 +0530

    Hero Page completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,103 +1,61 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
+  const router = useRouter();
 
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
+  const [isConnected, setIsConnected] = useState(false);
+
+  // Function to handle the "Join Us" button click
+  const handleJoinUsClick = () => {
+    // Assuming successful connection, set isConnected to true
+    setIsConnected(true);
+
+    // After successful connection, navigate to the dashboard
+    router.push("/dashboard");
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-black text-white">
+      <div className="flex-1 flex flex-col justify-center items-start px-8">
+        <div className="max-w-md">
+          {/* Larger BeraEvents title */}
+          <h1 className="text-6xl mb-4 font-bold">ShardEvents</h1>
+
+          {/* Gap between title and description */}
+          <div className="mb-6"></div>
+
+          <p className="mb-6 text-lg text-gray-400">
+            Discover and organize events related to shardeum blockchain.
+            ShardEvents is your gateway to the exciting world of decentralized
+            technology. Join us in shaping the future!
           </p>
 
-          <div className={styles.connect}>
-            <ConnectWallet />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+          {/* Render "Join Us" button */}
+          {!isConnected && (
+            <button
+              className="text-white px-4 py-2 rounded-lg cursor-pointer bg-purple-500"
+              onClick={handleJoinUsClick}
+            >
+              Join Us
+            </button>
+          )}
         </div>
       </div>
-    </main>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          alt="Bera Moon"
+          loading="lazy"
+          decoding="async"
+          data-nimg="1"
+          src="/logo.png"
+          className="object-cover max-w-full max-h-full"
+          style={{ maxHeight: "80vh", maxWidth: "100%" }}
+        />
+      </div>
+    </div>
   );
 };
 
